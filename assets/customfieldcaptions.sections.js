78703c3f9b172769a63e56be18c096a6bad35bc5
Phillip Gray <phill@thebold.nz>
2021-04-09 11:31:46 +1200

    Make sure the new max height is added to open elements and includes margin

--- assets/customfieldcaptions.sections.js
+++ assets/customfieldcaptions.sections.js
@@ -11,54 +11,52 @@
 -----------------------------------------------------------------------------*/
 
 	jQuery(document).ready(function() {
+	// jQuery(window).on('load', function() {
 		// Add a input field for every field instance
-		var $duplicator = jQuery('#fields-duplicator'),
-			$fields = $duplicator.find('.instance'),
+		const $duplicator = jQuery('#fields-duplicator');
+		const $fields = $duplicator.find('.instance');
+		// Get JSON data for the fields
+		const data = Symphony.Context.get('custom_captions');
+		// Template to clone for each field instance
+		const field_template = jQuery('<label class="cc-label" />')
+			.text(Symphony.Language.get('Custom caption'))
+			.append(
+				jQuery('<input />').attr('type', 'text')
+			);
+		// Inject the template into current $field
+		const addCaption = function($field, template) {
+			const $input = $field.find('div.content label:first');
+			const $input_container = $input.closest('div:not(.content, .invalid)');
 
-			// Get JSON data for the fields
-			data = Symphony.Context.get('custom_captions'),
+			if ($input_container.length) {
+				$input_container.after(template);
+			}
+			else {
+				$input.after(template);
+			}
+		};
+		// Inject template on the fly (as new fields as added)
+		const insertCaption = function($field) {
+			// If the field doesn't have a captions field already, add one
+			if($field.filter(':has(input[name*=custom_caption])').length == 0) {
+				const template = field_template.clone();
 
-			// Template to clone for each field instance
-			field_template = jQuery('<label />')
-				.text(Symphony.Language.get('Custom caption'))
-				.append(
-					jQuery('<input />')
-					.attr('type', 'text')
-				),
+				template
+					.find('input')
+					.attr('name', 'fields[' + ($field.index() - 1) + '][custom_caption]')
 
-			// Inject the template into current $field
-			addCaption = function($field, template) {
-				var $input = $field.find('div.content label:first'),
-					$input_container = $input.closest('div:not(.content, .invalid)');
-
-				if($input_container.length) {
-					$input_container.after(template);
-				}
-				else {
-					$input.after(template);
-				}
-			},
-
-			// Inject template on the fly (as new fields as added)
-			insertCaption = function($field) {
-				// If the field doesn't have a captions field already, add one
-				if($field.filter(':has(input[name*=custom_caption])').length == 0) {
-					var template = field_template.clone();
-
-					template.find('input')
-						.attr('name', 'fields[' + ($field.index() - 1) + '][custom_caption]')
-
-					addCaption($field, template);
-				}
-			};
+				addCaption($field, template);
+			}
+		};
 
 		// Initially run over the all the existing fields
 		$fields.each(function(i) {
-			var $field = jQuery(this),
-				field_id = $field.find(':hidden[name*=id]').val(),
-				template = field_template.clone();
+			const $field = jQuery(this);
+			const field_id = $field.find(':hidden[name*=id]').val();
+			const template = field_template.clone();
 
-			template.find('input')
+			template
+				.find('input')
 				.attr('name', 'fields[' + i + '][custom_caption]')
 
 			if(data != undefined && data[field_id] != undefined) {
@@ -66,7 +64,11 @@
 			}
 
 			addCaption($field, template);
-			$field.data('heightMax', parseInt($field.data('heightMax') + 55));
+
+			const new_height = parseInt($field.data('heightMax')) + parseInt($field.find('.cc-label').outerHeight()) + parseInt($field.find('.cc-label').css('margin-bottom'));
+
+			$field.data('heightMax', new_height);
+			if (!$field.hasClass('collapsed')) $field.css('max-height', new_height);
 		});
 
 		// Listen for when the duplicator changes [2.3]
